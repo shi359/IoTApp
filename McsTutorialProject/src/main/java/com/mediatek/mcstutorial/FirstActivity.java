260af632ae36f3a0c9d3634ec5040cd35e670755
Janet <shi359@gmail.com>
2016-06-20 00:10:35 +0800

    finish

--- McsTutorialProject/src/main/java/com/mediatek/mcstutorial/FirstActivity.java
+++ McsTutorialProject/src/main/java/com/mediatek/mcstutorial/FirstActivity.java
@@ -1,9 +1,9 @@
 package com.mediatek.mcstutorial;
 
-import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,18 +11,17 @@ import android.view.View;
 import android.widget.ImageButton;
 
 
-public class FirstActivity extends ActionBarActivity {
+public class FirstActivity extends FragmentActivity {
     ImageButton start;
     View  main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         main = getLayoutInflater().inflate(R.layout.activity_first, null);
         main.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        setContentView(R.layout.activity_first);
 
+        setContentView(R.layout.activity_first);
         Handler handler = new Handler();
 
         handler.postDelayed(new Runnable() {
--- McsTutorialProject/src/main/java/com/mediatek/mcstutorial/newActivity.java
+++ McsTutorialProject/src/main/java/com/mediatek/mcstutorial/newActivity.java
@@ -1,9 +1,14 @@
 package com.mediatek.mcstutorial;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,12 +20,16 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.gson.Gson;
 import com.mediatek.mcs.Utils.UIUtils;
 import com.mediatek.mcs.domain.McsDataChannel;
 import com.mediatek.mcs.domain.McsResponse;
 import com.mediatek.mcs.entity.DataChannelEntity;
 import com.mediatek.mcs.entity.DataPointEntity;
+import com.mediatek.mcs.entity.HistoryDataPointsEntity;
 import com.mediatek.mcs.entity.api.DeviceInfoEntity;
 import com.mediatek.mcs.entity.api.DeviceSummaryEntity;
 import com.mediatek.mcs.net.McsJsonRequest;
@@ -28,54 +37,89 @@ import com.mediatek.mcs.net.RequestApi;
 import com.mediatek.mcs.net.RequestManager;
 import com.mediatek.mcs.socket.McsSocketListener;
 import com.mediatek.mcs.socket.SocketManager;
+
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.RunnableFuture;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class newActivity extends AppCompatActivity {
+public class newActivity extends FragmentActivity {
 
     String mDeviceId = "";
     DeviceInfoEntity mDeviceInfo;
     McsDataChannel mDataChannel;
-    TextView comfort_tude;
-    TextView pressure_tude;
-    TextView allComfort_tude;
-    ImageButton ibtn;
+    TextView condition, comfort, health, all;
+    ImageButton btn;
+    Map<String, Float> data = new HashMap<String, Float>();
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private TimerTask timer;
+    private Runnable runner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_request);
-        this.comfort_tude = (TextView) findViewById((R.id.comfort));
-        this.pressure_tude = (TextView) findViewById((R.id.pressure));
-        this.allComfort_tude = (TextView) findViewById((R.id.all));
-        this.ibtn = (ImageButton) findViewById(R.id.imageButton);
+        this.condition = (TextView) findViewById((R.id.condition));
+        this.comfort = (TextView) findViewById((R.id.comfort));
+        this.health = (TextView) findViewById((R.id.health));
+        this.all = (TextView) findViewById(R.id.all);
+        btn = (ImageButton) findViewById(R.id.btn);
         final Intent intent = new Intent(newActivity.this, SessionActivity.class);
         startActivity(intent);
 
-       start();
-        ibtn.setOnClickListener(new View.OnClickListener()
-        {
-            @Override public void onClick(View view) {
-               Toast.makeText(newActivity.this, "click", Toast.LENGTH_LONG).show();
-               start();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        requestDevices();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                requestDeviceInfo(mDeviceId);
+            }}, 2000);
+//        Intent i = new Intent(newActivity.this, IntentServiceMCS.class);
+//        Bundle b = new Bundle();
+//        b.putString("id", mDeviceId);
+//        i.putExtras(b);
+//        startActivity(i);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               requestDeviceInfo(mDeviceId);
             }
         });
 
     }
 
-    @Override public boolean onCreateOptionsMenu(Menu menu) {
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
-    @Override public boolean onOptionsItemSelected(MenuItem item) {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -88,15 +132,15 @@ public class newActivity extends AppCompatActivity {
         String url = RequestApi.DEVICES;
         McsResponse.SuccessListener<JSONObject> successListener =
                 new McsResponse.SuccessListener<JSONObject>() {
-                    @Override public void onSuccess(JSONObject response) {
+                    @Override
+                    public void onSuccess(JSONObject response) {
                         List<DeviceSummaryEntity> summary = new Gson().fromJson(
                                 response.toString(), DeviceSummaryEntity.class).getResults();
 
                         if (summary.size() > 0) {
                             mDeviceId = summary.get(0).getDeviceId();
                         }
-
-                     //   printJson(response);
+                        //   printJson(response);
                     }
                 };
 
@@ -105,7 +149,8 @@ public class newActivity extends AppCompatActivity {
          * Default error message would be shown in logcat.
          */
         McsResponse.ErrorListener errorListener = new McsResponse.ErrorListener() {
-            @Override public void onError(Exception e) {
+            @Override
+            public void onError(Exception e) {
 
             }
         };
@@ -122,42 +167,32 @@ public class newActivity extends AppCompatActivity {
                 RequestApi.DEVICE
                         .replace("{deviceId}", deviceId),
                 new McsResponse.SuccessListener<JSONObject>() {
-                    @Override public void onSuccess(JSONObject response) {
+                    @Override
+                    public void onSuccess(JSONObject response) {
                         mDeviceInfo = UIUtils.getFormattedGson()
                                 .fromJson(response.toString(), DeviceInfoEntity.class)
                                 .getResults().get(0);
+                        List<DataChannelEntity> d = mDeviceInfo.getDataChannels();
+
+                        if(d != null){
+                            for(int i = 0; i < d.size(); i++){
+                                if(d.get(i).getDataPoint() != null){
+                                    String s = d.get(i).getDataChnId();
+                                    float value = Float.valueOf(d.get(i).getDataPoint().getValues().getValue());
+                                    data.put(s,value);
+
+                                }
+                            }
+                        }
+
+                        else Toast.makeText(newActivity.this, "No data to show", Toast.LENGTH_SHORT).show();
+                        calculate();
+
                     }
                 }
         );
 
         RequestManager.sendInBackground(request);
-    }
-
-    /**
-     * GET data channel
-     */
-    private void showDataChannel(DeviceInfoEntity deviceInfo) {
-        if (deviceInfo.getDataChannels().size() == 0) {
-            //tv_info.setText("data channel is empty, please create one");
-            return ;
-        }
-
-        /**
-         * Optional.
-         * Default message of socket update shows in log.
-         */
-        McsSocketListener socketListener = new McsSocketListener(
-                new McsSocketListener.OnUpdateListener() {
-                    @Override public void onUpdate(JSONObject data) {
-
-                    }
-                }
-        );
-        DataChannelEntity channelEntity = deviceInfo.getDataChannels().get(0);
-
-        mDataChannel = new McsDataChannel(deviceInfo, channelEntity, socketListener);
-        allComfort_tude.setText(mDataChannel.getDataPointEntity().getValues().getValue());
-      //  tv_info.setText(mDataChannel.getDataPointEntity().getValues().getValue());
     }
 
     /**
@@ -173,32 +208,129 @@ public class newActivity extends AppCompatActivity {
         SocketManager.disconnectSocket();
     }
 
-    private void start(){
-        requestDevices();
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                requestDeviceInfo(mDeviceId);
-            }
-        },3000);
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                showDataChannel(mDeviceInfo);
-            }
-        },5000);
+
+
+    private void calculate(){
+
+      float outA = data.get("fsr_3");
+      float inA = data.get("fsr_4");
+      float outB = data.get("fsr_2");
+      float inB = data.get("fsr_5");
+      float outC = data.get("fsr_1");
+      float inC = data.get("fsr_6");
+      float temp = data.get("temp");
+      float hum =  data.get("hum");
+      float A = inA + outA;
+      float B = inB + outB;
+      float C = inC + outC;
+      float in = inA + inB + inC;
+      float out = outA + outB + outC;
+
+      if(A == 0 && B == 0 && C == 0){
+          all.setText("尚無資料");
+          condition.setText("");
+          health.setText("");
+          comfort.setText("");
+      }
+
+      else{
+          float arch = B/(A+B+C);
+          float dir = in/out;
+          int total = 10;
+
+          if((temp > 28 && temp < 30) && hum > 60){
+              comfort.setText("不適");
+              total -= 3;
+          }
+          else if(temp > 30 && hum > 88){
+              comfort.setText("極度不適");
+              total -= 5;
+          }
+          else comfort.setText("舒適");
+
+          if(A > 0 && B > 0 && C > 0){
+              condition.setText("站立");
+              if(arch > 0.3){
+                  health.setText("足弓過低");
+                  total -= 5;
+              }
+              else if(arch < 0.15){
+                  health.setText("足弓過高");
+                  total -= 5;
+              }
+              else health.setText("健康");
+          }
+          else{
+              condition.setText("步行");
+              if(out == 0){
+                  health.setText("內八");
+                  total -= 5;
+              }
+              else if(in == 0){
+                  health.setText("外八");
+                  total -= 5;
+              }
+              else health.setText("健康");
+          }
+
+          all.setText(String.valueOf(C));
+      }
+
+      for(int i = 0; i < data.size(); i++) data.remove(i);
+
+
+
     }
-
-
     /**
      * Pretty-print a JSONObject.
      */
 //    private void printJson(JSONObject jsonObject) {
 //        try {
-//            tv_info.setText(jsonObject.toString(2));
+//            comfort_tude.setText(jsonObject.toString(2));
 //        } catch (JSONException e) {
 //            e.printStackTrace();
 //        }
 //    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "new Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.mediatek.mcstutorial/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "new Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.mediatek.mcstutorial/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+
 }
